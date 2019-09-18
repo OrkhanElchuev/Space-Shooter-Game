5f4bc8519db1741d5b20ce7e584e1743512d0b3e
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-18 23:30:38 +0200

    Enemy gets destroyed, enemy shooting functionality added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,63 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float healthPoints = 100.0f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minPeriodBetweenShots = 0.2f;
+    [SerializeField] float maxPeriodBetweenShots = 3.0f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Giving shotCounter random value in the range between min and max 
+        shotCounter = Random.Range(minPeriodBetweenShots, maxPeriodBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
     }
 
+    // Enemy shooting after random amount of time passed(between min and max)
+    public void CountDownAndShoot()
+    {
+        // Shot counter minus time that one frame takes 
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0.0f)
+        {
+            Shoot();
+            // Reset shot Counter to new value
+            shotCounter = Random.Range(minPeriodBetweenShots, maxPeriodBetweenShots);
+        }
+    }
+
+    // Method for handling enemy shooting
+    private void Shoot()
+    {
+        // Quaternion corresponds to "no rotatition" for instantiated object
+        GameObject enemyLaser = Instantiate(projectile,
+            transform.position, Quaternion.identity) as GameObject;
+        // -projectileSpeed to change reverse direction of shooting(shoot downwards)
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    // On collision of laser with Enemy deal the damage
     private void OnTriggerEnter2D(Collider2D other)
     {
         Damage damage = other.gameObject.GetComponent<Damage>();
+        ProcessHit(damage);
+    }
+
+    // Decrement health 
+    private void ProcessHit(Damage damage)
+    {
         healthPoints -= damage.GetDamage();
+        // Destroy object when health <= 0
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
