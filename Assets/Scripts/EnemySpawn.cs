bb61d941adf556180935dbd4e5bc87e834594f40
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-18 22:01:32 +0200

    Multiple enemy spawning function created

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
+    private int startWave = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        var currentWave = waveConfigs[startWave];
+        StartCoroutine(SpawnAllEnemies(currentWave));
     }
 
-    // Update is called once per frame
-    void Update()
+    // For spawning enemies in current wave
+    private IEnumerator SpawnAllEnemies(WaveConfig waveConfig)
     {
-        
+        // i corresponds to enemy count
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+            // Creating enemies and spawning after specified delay
+            Instantiate(waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+
+        }
     }
 }
