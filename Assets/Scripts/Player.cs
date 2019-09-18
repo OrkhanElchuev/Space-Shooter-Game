8f9a947e31b65b056e31eeddcf85db4942b96ff8
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-18 17:49:46 +0200

    movement of spaceship with arrows and WASD keyboards is created

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    // Frame Rate independent 2D object moving function
+    private void MovePlayer()
+    {
+        // Horizontal provides functionality for moving object with AD or arrows
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPosition = transform.position.x + deltaX;
+        // Vertical provides functionality for moving object with WS or arrows
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPosition = transform.position.y + deltaY;
+        
+        transform.position = new Vector2(newXPosition, newYPosition);
+    }
+}
