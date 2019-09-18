fddd449e6a6c9727dc9c8a07e3df42148bf17563
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-18 22:54:35 +0200

    Enemy health added, laser deal damage function added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float healthPoints = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Damage damage = other.gameObject.GetComponent<Damage>();
+        healthPoints -= damage.GetDamage();
+    }
+}
