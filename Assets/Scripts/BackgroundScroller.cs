f5f8211735cd91385027bf8aa62685788baa0e0d
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-19 01:03:27 +0200

    Continuously scrolling background effect added

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    private Material myMaterial;
+    private Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        // Scrolling up on Y axis
+        offSet = new Vector2(0.0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Frame rate independent moving speed
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
