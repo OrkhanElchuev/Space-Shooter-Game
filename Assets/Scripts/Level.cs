38b9dcd3412bfb306df1ae06b58ff31c882d9797
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-19 21:09:48 +0200

    Level script added, to load Start and Game Over Menus

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadStartMenu()
     {
-        
+        SceneManager.LoadScene("Start Menu");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGame()
     {
-        
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
